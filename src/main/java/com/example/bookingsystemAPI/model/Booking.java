240995df287e3bee33ba8fc966562951db37304c
Lisa <lisafalth05@gmail.com>
2025-11-22 13:00:56 +0100

    Remove unnecessary import and add improvement suggestion

--- src/main/java/com/example/bookingsystemAPI/model/Booking.java
+++ src/main/java/com/example/bookingsystemAPI/model/Booking.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Booking {
+public class Booking { // Lägg till validering i framtiden :)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Databasen sköter ID automatiskt
     private Long id;
